f2ad3d7a318d913c026c896483ac898ed82cb4cc
coderhook <abel1987@hotmail.es>
2019-06-29 11:42:19 +0200

    Added a post request for our playlist, now we can add playlists

--- index.js
+++ index.js
@@ -1,10 +1,15 @@
 const express = require('express') 
+const bodyParser = require('body-parser')
+
 const db = require('./db.js')
+
 const playlist = require('./playlist/model.js')
 const playlistRouter = require('./playlist/router.js')
 
 const app = express()
+const jsonParser = bodyParser.json()
 
+app.use(jsonParser)
 app.use(playlistRouter);
 
 const port = process.env.PORT || 4000;
--- playlist/router.js
+++ playlist/router.js
@@ -1,13 +1,20 @@
 const Router = require('express')
 const Playlist = require('./model')
 
-const router = new Router()
+const router = new Router();
 
 router.get('/playlist', (req, res, next) => {
   Playlist
     .findAll()
     .then(playlists => res.status(200).send(playlists))
-    .catch(err => next(err))
+    .catch(err => res.status(500).next(err))
+});
+
+router.post('/playlist', (req, res, next) => {
+  Playlist
+    .create(req.body)
+    .then(newData => res.status(201).send(newData))
+    .catch(err => res.status(422).next(err))
 })
 
 module.exports = router
