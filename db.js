d99058b8d2ae0f845c408ddaec9b70a3943aad6f
coderhook <abel1987@hotmail.es>
2019-06-29 10:15:12 +0200

    deplying to heroku

---
+++ db.js
@@ -0,0 +1,12 @@
+const Sequelize = require('sequelize')
+
+const databaseUrl = process.env.DATABASE_URL || 'postgres://postgres:pedro@localhost:5432/postgres'
+
+const sequelize =  new Sequelize(databaseUrl);
+
+sequelize
+  .sync()
+  .then(() => console.log('Database Schema Updated!'))
+  .catch(console.error)
+
+module.exports = sequelize
